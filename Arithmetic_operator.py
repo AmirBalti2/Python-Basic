104a94753453e79b33829ebd7511f0219b5ebb04
AmirBalti2 <61861267+AmirBalti2@users.noreply.github.com>
2020-03-09 12:14:58 +0500

    Add files via upload

---
+++ Arithmetic_operator.py
@@ -0,0 +1,8 @@
+First_Number=int(input("Enter the number "))
+Second_Number=int(input("Enter the number "))
+print(f"The sum of two number is {First_Number+Second_Number}",end=' \n')
+print(f"The Multiple of two number is {First_Number*Second_Number}",end=' \n')
+print(f"The Divison of two number is {int(First_Number/Second_Number)}",end=' \n')
+print(f"The Mode of two number is {First_Number%Second_Number}",end=' \n')
+
+
